ad3d6432dddcafe99068e5e1817de7ccc46ac980
superkrasotkaaru <157337099+superkrasotkaaru@users.noreply.github.com>
2026-04-14 21:47:40 +0500

    Add files via upload

---
+++ api.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/categories', [CategoryController::class, 'index']);
+    Route::post('/categories', [CategoryController::class, 'store']);
+    Route::get('/categories/{id}', [CategoryController::class, 'show']);
+    Route::put('/categories/{id}', [CategoryController::class, 'update']);
+    Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+
+    Route::get('/products/{id}/categories', [CategoryController::class, 'getProductCategories']);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
